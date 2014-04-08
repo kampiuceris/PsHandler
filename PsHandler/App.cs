d4ee1d41ab84f2936a972a9757fc3c51db577d3f
kampiuceris <kampiuceris@gmail.com>
2014-04-08 18:23:44 +0300

    Deleted obsolete code

--- PsHandler/App.cs
+++ PsHandler/App.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Drawing;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows;
-using System.Windows.Interop;
-using System.Windows.Shapes;
-using Rectangle = System.Drawing.Rectangle;
+
 
 namespace PsHandler
 {
